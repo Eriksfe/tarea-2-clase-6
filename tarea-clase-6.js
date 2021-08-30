5d3fa6fe030682925e46002af6ea1347fad6001a
Eric <schnider.erik@gmail.com>
2021-08-30 16:15:10 -0300

    agrega comentario con la tarea
    se añade la tarea al principio del código como control que se hizo lo que se pidió

--- tarea-clase-6.js
+++ tarea-clase-6.js
@@ -1,3 +1,13 @@
+/*
+TAREA:
+Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para
+completar el salario anual de cada integrante de la familia que trabaje.
+Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual,
+menor salario anual, salario anual promedio y salario mensual promedio.
+
+Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
+*/
+
 const $botonAgregar = document.querySelector("#agrega-campo");
 const $botonQuitar = document.querySelector("#quita-campo");
 const $botonCalcular = document.querySelector("#calcular");
